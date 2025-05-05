a8cafc64fa10a43844600738aa861f85b74fd4a9
SAHIL KOSHRIYA <42277459-sahilkoshriya82@users.noreply.replit.com>
2025-05-05 10:10:10 +0000

    basic java sript

---
+++ 01_JS_basics/Var_let_const.js
@@ -0,0 +1,16 @@
+//  var let const 
+//  there are many probleam before ES6 Ecma script 6 DOC that why there is deff between var and  let const 
+/* 
+   1.Var is fuction scope 
+   2.let and const is block scope
+   3. var can be redeclared and updated
+   4. let can be redeclared and updated
+   5. const can not be redeclared and updated
+   6. var is gloabl scope and let and const is block scope
+   7. var is hoisted and let and const is not hoisted but lete and const is hoisted but not initialized
+   8. let and const are trape in temporal dead zone
+   10. temporal dead zone is the time between the hoisting and initialization of let and const
+   
+
+   */
+//  ******* Note = alt + click to selct multiple lines 
---
+++ 01_JS_basics/index.js
@@ -0,0 +1,27 @@
+"use strict";  //  to use newest syntax nad code strictly " sending msg to the Js Engine to treat all js code as newer version "
+// alert("ion node Js not in browser")
+
+// when use semicolon and when not
+  /*1. most imp is code redability 
+    2. Documentation Read from MDN  -by mozilla firefox
+    3. But original documentation is at ECMA international JS documentation 
+
+  */
+// Data types
+  //primitive Datatype
+  /*
+   1.number 
+   2.bigInt 
+   3.String
+   4. Null = stand alone value a special type 
+   5. undefined 
+   6. Symboll to find uniqueness
+   */
+
+   //
+
+     //*1 object
+
+     console.log(typeof(null)); // O/P - object
+     console.log(typeof(undefined)); // o/p -undefined 
+
