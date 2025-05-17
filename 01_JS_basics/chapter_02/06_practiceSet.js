4f725785064c58f752e04e1bebb08352f3f42304
SAHIL KOSHRIYA <134610143+SAHILKOSHRIYA@users.noreply.github.com>
2025-05-17 22:14:06 +0000

    practiceset

--- 01_JS_basics/chapter_02/06_practiceSet.js
+++ 01_JS_basics/chapter_02/06_practiceSet.js
@@ -12,3 +12,50 @@ if(age>10 && age<20) {
 }
 
 //Q2 : Demonstrate the use of switch case statements in a JavaScript program.
+let age1 = Number.parseInt(prompt("What is your age? "));
+switch(age1)
+  {
+    case 12 :
+      console.log("You are 12 years old")
+      break
+    case 13  :
+      console.log("You are 13 years old")
+      break
+      case 14  :
+      console.log("You are 14 years old")
+      break
+      case 15:
+      console.log("You are 15 years old")
+      break
+      case 16  :
+      console.log("You are 16 years old")
+      break
+  
+default:
+console.log("You are not eligible")
+  }
+
+//Q3 : Write a JavaScript program to find whether a number is divisible by 2 and 3
+
+let num = Number.parseInt(prompt("Enter a number: "));
+if(num%2==0&&num%3==0)  
+{
+  console.log("The number is divisible by 2 and 3")
+}
+else{
+  console.log("The number is not divisible by 2 and 3")
+}
+
+//Q4 : Write a JavaScript program to find whether a number is divisible by either 2 or 3
+let number = Number.parseInt(prompt("Enter a number: "));
+if(number%2==0 ||number%3==0)  
+{
+  console.log("The number is divisible  either by 2 and 3")
+}
+else{
+  console.log("The number is not divisible by  both 2 and 3")
+}
+//Q 5: Print "you can drive" or "you cannot drive" based on age being greater than 18 using ternary operator
+let age2 = Number.parseInt(prompt("What is your age? "));
+let a = age2 > 18 ? "You can drive" : "You cannot drive"
+console.log(a)
