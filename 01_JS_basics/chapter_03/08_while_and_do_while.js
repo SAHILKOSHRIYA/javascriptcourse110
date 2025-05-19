b4aef90442bd41e8dcc6c70aa4ae86a8cf880179
SAHIL KOSHRIYA <134610143+SAHILKOSHRIYA@users.noreply.github.com>
2025-05-19 13:46:45 +0000

    loops_while_dowhile

--- 01_JS_basics/chapter_03/08_while_and_do_while.js
+++ 01_JS_basics/chapter_03/08_while_and_do_while.js
@@ -1,5 +1,9 @@
 console.log("Chapter 03: Loops\n")
-//2. while loop : is used to execute a block of code repeatedly as long as a specified condition is true.
+const prompt= require('prompt-sync')();
+
+ 
+//2. while loop : is used to execute a block of code repeatedly as long as a specified condition is true. 
+//ek hi condtion ko bar bar chalane k liye jab tak vo false nhi ho jaye
 /*
 Syntax: while(condition) 
 { 
@@ -14,3 +18,13 @@ while(i<n)
     console.log(n)
     i++;
   }
+
+//3. do while loop : is used to execute a block of code repeatedly as long as a specified condition is true. but it will execute at least once even if the condition is false.
+let n1 = prompt("Enter the value of n")
+n1 = Number.parseInt(n)
+let j = 10 ;
+do // atleat ek bar karo
+{
+    console.log(j)
+    j++;
+}while(j<n1)
