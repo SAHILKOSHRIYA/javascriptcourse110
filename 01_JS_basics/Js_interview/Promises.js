44df9dec6c0e663e9f9e1ec03384931f5a78a270
SAHIL KOSHRIYA <134610143+SAHILKOSHRIYA@users.noreply.github.com>
2025-07-05 11:10:45 +0000

    resolving call back hell

---
+++ 01_JS_basics/Js_interview/Promises.js
@@ -0,0 +1,5 @@
+// promises are used to handle asynchronous operations in js.
+// Syntax: let promise = new Promise(function(resolve, reject)
+// promise is an object that represents the eventual completion or failure of an asynchronous operation.
+// promise has three states: pending, fulfilled, rejected.
+console.log("Start");
